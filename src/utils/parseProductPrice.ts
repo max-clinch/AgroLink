9fd7fdf1ea222f3b8c04e52bacb57d4ea0bf7875
max-clinch <74224236+max-clinch@users.noreply.github.com>
2023-11-30 14:20:35 +0100

    modifications

--- src/utils/parseProductPrice.ts
+++ src/utils/parseProductPrice.ts
@@ -5,7 +5,8 @@ export const parseEthToWei = (val: number) => {
   return val * 10 ** 18;
 };
 
-export const parseWeiToEth = (val: number) => {
-  if (!val) return "0";
-  return ethers.utils.formatEther(val.toString());
-};
+//export const parseWeiToEth = (val: number) => {
+//  if (!val) return "0";
+//  return ethers.utils.formatEther(val.toString());
+//};
+
